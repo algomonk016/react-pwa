35862ef1146e8f49af9478b83cfcf72c7c594ccd
algomonk016 <stshandilya0820@gmail.com>
2023-06-01 06:39:43 +0530

    offline mode for static app done

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,26 @@
-const CACHE_NAME = 'shirorororo'
+const CACHE_NAME = 'shirorororo';
 
 this.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      cache.addAll([
+      return cache.addAll([
         '/static/js/bundle.js',
         '/static/js/main.chunk.js',
         '/static/js/0.chunk.js',
         '/index.html',
         '/',
-      ])
+        '/static/css/main.css', // Add CSS files
+        '/static/images/logo.png', // Add image files
+        '/static/fonts/font.woff', // Add font files
+      ]);
     })
-  )
-})
+  );
+});
+
+this.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
