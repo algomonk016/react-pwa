34ce49264c39ab9a3a0eb503dcb3857955fc3369
algomonk016 <stshandilya0820@gmail.com>
2023-07-04 10:21:33 +0530

    integrated indexedDB

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,9 +1,17 @@
-import React, { FC, useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
+import indexedDb from '../../services/dbInstance';
 
 const Users: FC = () => {
+  const [users, setUsers] = useState<any[]>([]);
+  const [mode, setMode] = useState<string>('online');
 
-  const [users, setUsers] = useState<any[]>([])
-  const [mode, setMode] = useState<string>('online')
+  // indexed db
+  const [cnt, setCnt] = useState<number>(0);
+  const [users2, setUsers2] = useState<any[]>([]);
+
+  useEffect(() => {
+    getData();
+  }, [])
 
   useEffect(() => {
     const url = 'https://random-data-api.com/api/v2/users?size=2';
@@ -19,13 +27,54 @@ const Users: FC = () => {
         const data = JSON.parse(localStorage.getItem('users') || '')
         if(!!data) setUsers(data)
       })
-  }, [])  
+  }, []) 
+
+  async function addData() {
+    const dataToAdd = { id: cnt, name: 'Shivesh Tiwari', age: 23 };
+    const success = await indexedDb.putValue('myTable', dataToAdd);
+    if (success) {
+      console.log('Data added successfully');
+      getData();
+    } else {
+      console.error('Failed to add data');
+    }
+  }
+  
+  async function getData() {
+    const data = await indexedDb.getAllValue<any>('myTable');
+    setUsers2(data);
+    if(data.length === 0) setCnt(1);
+    else setCnt(data.at(-1).id + 1);
+  }
+
+  async function deleteData(id: number) {
+    await indexedDb.deleteValue('myTable', id);
+    getData();
+  }
 
   return (
     <div>
       <h1>yohohoho - {mode}</h1>
 
       <div>
+        <button onClick={addData}>Add</button>
+        <p style={{ fontSize: 24, fontWeight: '800' }}>indexedDB users</p>
+
+        <div>
+          {
+            users2.map(user => (
+              <div key={`users-2-${user.id}`}>
+                {user.id}-{user.name}
+
+                <button onClick={() => deleteData(user.id)} style={{ margin: '10px' }}>X</button>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+
+      <div>
+        <p style={{ fontSize: 24, fontWeight: '800' }}>Api fetched users</p>
         { 
           users.map(user => (
             <div key={user.id}>
---
+++ src/services/db.ts
@@ -0,0 +1,128 @@
+import { IDBPDatabase, openDB } from 'idb';
+
+class IndexedDb {
+  private database: string;
+  private db: IDBPDatabase | null;
+
+  constructor(database: string) {
+    this.database = database;
+    this.db = null;
+  }
+
+  public async openDatabase(tableNames: string[]): Promise<boolean> {
+    try {
+      this.db = await openDB(this.database, 1, {
+        upgrade(db: IDBPDatabase) {
+          for (const tableName of tableNames) {
+            if (!db.objectStoreNames.contains(tableName)) {
+              db.createObjectStore(tableName, { autoIncrement: true, keyPath: 'id' });
+            }
+          }
+        },
+      });
+      return true;
+    } catch (error) {
+      console.error('Failed to open database:', error);
+      return false;
+    }
+  }
+
+  public async getValue<T>(tableName: string, id: number): Promise<T | undefined> {
+    if (!this.db) {
+      console.error('Database is not open.');
+      return undefined;
+    }
+
+    const tx = this.db.transaction(tableName, 'readonly');
+    const store = tx.objectStore(tableName);
+    try {
+      const result = await store.get(id);
+      console.log('Get Data ', JSON.stringify(result));
+      return result;
+    } catch (error) {
+      console.error(`Failed to get value from ${tableName}:`, error);
+      return undefined;
+    }
+  }
+
+  public async getAllValue<T>(tableName: string): Promise<T[]> {
+    if (!this.db) {
+      console.error('Database is not open.');
+      return [];
+    }
+
+    const tx = this.db.transaction(tableName, 'readonly');
+    const store = tx.objectStore(tableName);
+    try {
+      const result = await store.getAll();
+      console.log('Get All Data', JSON.stringify(result));
+      return result;
+    } catch (error) {
+      console.error(`Failed to get all values from ${tableName}:`, error);
+      return [];
+    }
+  }
+
+  public async putValue<T>(tableName: string, value: T): Promise<boolean> {
+    if (!this.db) {
+      console.error('Database is not open.');
+      return false;
+    }
+
+    const tx = this.db.transaction(tableName, 'readwrite');
+    const store = tx.objectStore(tableName);
+    try {
+      await store.put(value);
+      console.log('Put Data ', JSON.stringify(value));
+      return true;
+    } catch (error) {
+      console.error(`Failed to put value in ${tableName}:`, error);
+      return false;
+    }
+  }
+
+  public async putBulkValue<T>(tableName: string, values: T[]): Promise<T[]> {
+    if (!this.db) {
+      console.error('Database is not open.');
+      return [];
+    }
+
+    const tx = this.db.transaction(tableName, 'readwrite');
+    const store = tx.objectStore(tableName);
+    try {
+      for (const value of values) {
+        await store.put(value);
+        console.log('Put Bulk Data ', JSON.stringify(value));
+      }
+      return await this.getAllValue(tableName);
+    } catch (error) {
+      console.error(`Failed to put bulk values in ${tableName}:`, error);
+      return [];
+    }
+  }
+
+  public async deleteValue(tableName: string, id: number): Promise<boolean> {
+    if (!this.db) {
+      console.error('Database is not open.');
+      return false;
+    }
+
+    const tx = this.db.transaction(tableName, 'readwrite');
+    const store = tx.objectStore(tableName);
+    try {
+      const result = await store.get(id);
+      if (!result) {
+        console.log('Id not found', id);
+        return false;
+      }
+      await store.delete(id);
+      console.log('Deleted Data', id);
+      return true;
+    } catch (error) {
+      console.error(`Failed to delete value from ${tableName}:`, error);
+      return false;
+    }
+  }
+}
+
+export default IndexedDb;
